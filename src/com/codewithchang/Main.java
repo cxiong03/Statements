f8711b378800eaf78cc00997422de8a42bf65921
Chang <cnxiong03@gmail.com>
2021-01-04 15:20:31 -0500

    java statements

---
+++ src/com/codewithchang/Main.java
@@ -0,0 +1,12 @@
+package com.codewithchang;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // These are all statements
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a complete statement");
+    }
+}
